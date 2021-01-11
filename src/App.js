d050c6a613e5162cf2df5e889b7eb28c58bcf62a
ZalotBabyZ <babyzalot@gmail.com>
2021-01-11 14:20:43 +0700

    pdf resume download

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   faExternalLinkAlt,
   faUnlink,
   faCircle,
+  faDownload,
 } from '@fortawesome/free-solid-svg-icons';
 import CertificationCard from './components/CertificationCard';
 import ContactCard from './components/ContactCard';
@@ -17,11 +18,19 @@ import ProjectCard from './components/ProjectCard';
 import MiniprojectCard from './components/MiniprojectCard';
 import ProfileCard from './components/ProfileCard';
 import SkillCard from './components/SkillCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-library.add(fab, faPhoneAlt, faEnvelopeOpenText, faAngleDoubleRight, faExternalLinkAlt, faUnlink, faCircle);
+library.add(fab, faPhoneAlt, faEnvelopeOpenText, faAngleDoubleRight, faExternalLinkAlt, faUnlink, faCircle, faDownload);
 function App() {
   return (
     <div className="App">
+      <a
+        href="https://drive.google.com/file/d/1CqRMUDxXKcUBRdZF9b7OwuVZmtYcbtja/view?usp=sharing"
+        className="print-resume"
+      >
+        <FontAwesomeIcon icon="download" />
+        <div className="print-resume-clue"> pdf download </div>
+      </a>
       <div className="page-header">
         <h2>NOTHING CAN STOP YOU ONCE YOU HAVE A CLEAR VISION OF WHAT YOU WANT.</h2>
       </div>
